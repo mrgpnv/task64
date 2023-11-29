4758a7442f329bf07c1984e430c7b331169d874e
Геннадий Панов <genapanov@MacBook-Air-Gennadij.local>
2023-11-29 16:31:18 +0300

    completed task 64

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+using System;
+
+class Program
+{
+    static void numbers(int m, int n) 
+    {
+        Console.WriteLine(m);
+        if (m < n) 
+        {
+            numbers(m + 1, n);
+        }
+    }
+    
+    static int Prompt(string Word) 
+    {
+        Console.WriteLine(Word);
+        return int.Parse(Console.ReadLine());
+    }
+    
+    static void Main() 
+    {
+        int m = Prompt("Введите первое число");
+        int n = Prompt("Введите второе число");
+        numbers(m, n);
+    }
+}
